3bd8cabf58791cdb8659d610ed486f64cdd67826
SebTota <kidokyo1@gmail.com>
2021-04-09 15:00:14 -0400

    finished parsing RSS and downloading image

--- Podcast-Player/ViewController.swift
+++ Podcast-Player/ViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 import AVFoundation
+import FeedKit
 
 class ViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer = AVAudioPlayer()
-
+    @IBOutlet weak var podcastImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let feedURL = URL(string: "https://feeds.megaphone.fm/stufftheydontwantyoutoknow")
+        
+        let parser = FeedParser(URL: feedURL!)
+        parser.parseAsync(queue: DispatchQueue.global(qos: .userInitiated)) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                    case .success(let feed):
+                        let photoURL = URL(string: feed.rssFeed!.image!.url!)!
+                        self.downloadImage(from: photoURL)
+                        
+                        print(feed.rssFeed!.title)
+                        
+                    case .failure(let error):
+                        print(error)
+                    }
+            }
+        }
         
         let audio = Bundle.main.path(forResource: "podcast", ofType: "mp3")
         
@@ -26,6 +46,23 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    func downloadImage(from url: URL) {
+        print("Download Started")
+        getData(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() {
+                self.podcastImage.image = UIImage(data: data)
+            }
+        }
+    }
+
 
     @IBAction func playButtonPressed(_ sender: UIButton) {
         audioPlayer.play()
