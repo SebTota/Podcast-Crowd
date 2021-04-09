5c92bbecd1b38a374deb2d7679130d6bb2002b98
SebTota <kidokyo1@gmail.com>
2021-04-09 19:19:18 -0400

    modularized episode download

--- Podcast-Player/Episode.swift
+++ Podcast-Player/Episode.swift
@@ -6,17 +6,82 @@
 //
 
 import Foundation
+import AVFoundation
 
 class Episode {
     
     private var title: String
+    private var audioUrl: URL
+    private var audioPath: URL
     private var description: String
     
-    init(title: String, description: String) {
+    init(title: String, audioUrl: URL, description: String, showTitle: String) {
         self.title = title
         self.description = description
+        self.audioUrl = audioUrl
+        
+        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        // self.audioPath = documentsDirectoryURL.appendingPathComponent(showTitle)
+        self.audioPath = documentsDirectoryURL.appendingPathComponent(audioUrl.lastPathComponent)
     }
     
+    /*
+     * Download the audio file for this episode to local storage
+     */
+    private func downloadEpisode(callback: @escaping (Bool) -> ()) {
+        URLSession.shared.downloadTask(with: self.audioUrl) { location, response, error in
+            guard let location = location, error == nil else { return }
+            do {
+                print("Downloaded episode to local storage")
+                try FileManager.default.moveItem(at: location, to: self.audioPath)
+                callback(true)
+            } catch {
+                print("Error downloading episode")
+                print(error)
+                callback(false)
+            }
+        }.resume()
+    }
     
+    /*
+     * Check if the audio file for this episode exists in local storage
+     * @param   Bool    true: file exists in local storage, false: file doesn't exist in local storage
+     */
+    private func checkIfEpisodeExists() -> Bool {
+        if FileManager.default.fileExists(atPath: audioPath.path) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    /*
+     * Read the audio file for this episode from local storage
+     */
+    private func getEpisodeFromLocalStorage() -> AVAudioPlayer? {
+        do {
+            let audioPlayer: AVAudioPlayer = try AVAudioPlayer(contentsOf: audioPath)
+            return audioPlayer
+        } catch {
+            return nil
+        }
+    }
+    
+    /*
+     * Retrieve the audio for this episode
+     */
+    func getEpisode(callback: @escaping (AVAudioPlayer?) -> ()) {
+        if checkIfEpisodeExists() == true {
+            callback(self.getEpisodeFromLocalStorage())
+        } else {
+            downloadEpisode { (success: Bool) in
+                if success == true {
+                    callback(self.getEpisodeFromLocalStorage())
+                } else {
+                    callback(nil)
+                }
+            }
+        }
+    }
     
 }
--- Podcast-Player/ViewController.swift
+++ Podcast-Player/ViewController.swift
@@ -47,9 +47,25 @@ class ViewController: UIViewController {
 //                        }
                         
                         if let first = feed.rssFeed?.items?.first {
-                            if let url = first.enclosure?.attributes?.url {
-                                print(url)
-                                self.downloadAudio2(url: URL(string: url))
+                            
+                            if let url = first.enclosure?.attributes?.url, let description = first.description, let title = first.title {
+                                
+                                let episode = Episode(title: title, audioUrl: URL(string: url)!, description: description, showTitle: "test")
+                                episode.getEpisode { (audioPlayer: AVAudioPlayer?) in
+                                    DispatchQueue.main.async() {
+                                        if let audioPlayer = audioPlayer {
+                                            self.audioPlayer = audioPlayer
+                                            print("Ready to play audio")
+                                            do {
+                                                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
+                                            } catch {
+                                                print(error)
+                                            }
+                                        } else {
+                                            print("Error downloading file")
+                                        }
+                                    }
+                                }
                             }
                         }
                         
@@ -82,85 +98,6 @@ class ViewController: UIViewController {
                 self.podcastImage.image = UIImage(data: data)
             }
         }
-    }
-    
-    func downloadAudio(from url: URL) {
-        print("Download Started")
-        getData(from: url) { data, response, error in
-            guard let data = data, error == nil else { return }
-            print("Download finished")
-            DispatchQueue.main.async {
-                
-                let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("podcastTestFile")
-                try? data.write(to: path)
-                
-                do  {
-                
-                    let audio = try Bundle.main.path(forResource: String(contentsOf: path), ofType: "mp3")
-                    self.audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: audio!))
-                    // self.audioPlayer = try AVAudioPlayer(data: data)
-                    try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
-                } catch {
-                    print(error)
-                }
-            }
-        }
-    }
-    
-    func downloadAudio2(url: URL?) {
-        if let audioUrl = url {
-
-            // then lets create your document folder url
-            let documentsDirectoryURL =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-
-            // lets create your destination file url
-            let destinationUrl = documentsDirectoryURL.appendingPathComponent(audioUrl.lastPathComponent)
-            print(destinationUrl)
-
-            // to check if it exists before downloading it
-            if FileManager.default.fileExists(atPath: destinationUrl.path) {
-                print("The file already exists at path")
-                
-                
-                do  {
-                    //let audio = try Bundle.main.path(forResource: String(contentsOf: destinationUrl), ofType: "mp3")
-                    self.audioPlayer = try AVAudioPlayer(contentsOf: destinationUrl)
-                    // self.audioPlayer = try AVAudioPlayer(data: data)
-                    try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
-                } catch {
-                    print(error)
-                }
-
-            } else {
-
-                // you can use NSURLSession.sharedSession to download the data asynchronously
-                URLSession.shared.downloadTask(with: audioUrl) { location, response, error in
-                    guard let location = location, error == nil else { return }
-                    do {
-                        // after downloading your file you need to move it to your destination url
-                        try FileManager.default.moveItem(at: location, to: destinationUrl)
-                        print("File moved to documents folder")
-                        
-                        do  {
-                            //let audio = try Bundle.main.path(forResource: String(contentsOf: destinationUrl), ofType: "mp3")
-                            self.audioPlayer = try AVAudioPlayer(contentsOf: destinationUrl)
-                            // self.audioPlayer = try AVAudioPlayer(data: data)
-                            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
-                        } catch {
-                            print(error)
-                        }
-                        
-                    } catch {
-                        print(error)
-                    }
-                }.resume()
-            }
-            
-            
-            
-            
-        }
-
     }
 
 
