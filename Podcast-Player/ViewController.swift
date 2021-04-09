b62280ce89f3f4c0cad227b45769d75481801c65
SebTota <kidokyo1@gmail.com>
2021-04-09 14:17:21 -0400

    added basic play/pause audio player

--- Podcast-Player/ViewController.swift
+++ Podcast-Player/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let audio = Bundle.main.path(forResource: "podcast", ofType: "mp3")
+        
+        do  {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: audio!))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
+        } catch {
+            print(error)
+        }
+        
     }
 
-
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        audioPlayer.play()
+    }
+    
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+        audioPlayer.pause()
+    }
 }
 
